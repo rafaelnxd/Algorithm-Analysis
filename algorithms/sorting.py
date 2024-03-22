c8676b9591a483345ee147f488658a554d0cddf3
rafaelnxd <rafaelnovaes46@outlook.com>
2024-03-21 22:32:08 -0300

    test

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -6,3 +6,12 @@ class Sorting:
             for j in range(0, n - i - 1):
                 if arr[j] > arr[j + 1]:
                     arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        return arr
+    
+
+
+# arr1 = [3, 2, 1, 5, 4]
+# sorted_arr1 = Sorting.bubble_sort(arr1)
+# print(arr1)
+    
+    
---
+++ tests/test_sorting.py
@@ -0,0 +1,3 @@
+import unittest
+from algorithms.sorting import Sorting
+
